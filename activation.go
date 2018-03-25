c3b43c011f560a192b58449981d543ea69c7e227
reficul31 <reficul31@gmail.com>
2018-03-25 22:16:00 +0530

    Defining the activation functions

---
+++ activation.go
@@ -0,0 +1,21 @@
+package mlp
+
+import "math"
+
+var sigmoid = ActivationFunction{
+	function: func(x float64) float64 {
+		return 1 / (1 + math.Exp(-1*x))
+	},
+	dfunction: func(y float64) float64 {
+		return y*(1-y)
+	},
+}
+
+var tanh = ActivationFunction{
+	function: func(x float64) float64 {
+		return math.Atanh(x)
+	},
+	dfunction: func(y float64) float64 {
+		return 1-y*y
+	},
+}
