3525d529d01dda1642ac8739955df18dc7f930ad
reficul31 <reficul31@gmail.com>
2018-03-25 22:17:53 +0530

    Defining the types

---
+++ file.go
@@ -0,0 +1,77 @@
+package mlp
+
+import (
+	"os"
+	"bufio"
+	"encoding/csv"
+	"strconv"
+	"io"
+	"strings"
+	"path/filepath"
+)
+
+func ReadData(filename string, stringHandler func(string) string) ([][]float64, error) {
+	var data [][]float64
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return data, err
+	}
+
+	if strings.Compare(filepath.Ext(filename), ".csv") != 0 {
+		return data, ErrOnlyCSVFiles
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(bufio.NewReader(file))
+	if err != nil {
+		return data, err
+	}
+
+	for {
+		row, err := reader.Read()
+		if err == io.EOF {
+			break;
+		}
+
+		dataRow := make([]float64, len(row))
+		for i := 0; i < len(row); i++ {
+			row[i] = stringHandler(row[i])
+			dataRow[i], err = strconv.ParseFloat(row[i], 64)
+			if err != nil {
+				return data, err
+			}
+		}
+		data = append(data, dataRow)
+	}
+
+	return data, nil
+}
+
+func WriteData(filename string, data [][]float64) error {
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+
+	if strings.Compare(filepath.Ext(filename), ".csv") != 0 {
+		return ErrOnlyCSVFiles
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, row := range data {
+		writeData := make([]string, len(row))
+		for i := 0; i < len(row); i++ {
+			writeData[i] = strconv.FormatFloat(row[i], 'f', 6, 64)
+		}
+    	err := writer.Write(writeData)
+    	if err != nil {
+    		return err
+    	}
+	}
+	return err
+}
